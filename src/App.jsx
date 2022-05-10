347edf7f73492225268a4dc76a5ff42ea44330f9
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-10 10:33:33 +0200

    onclick for moving right, left, center added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,26 @@ import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hallo!</div>;
+  const [position, setPosition] = useState("Center");
+  return (
+    <div className="App">
+      <h2>Move an element left, right, center when button is pressed</h2>
+      <div className="controls">
+        <button className="left" onClick={() => setPosition("Left")}>
+          Left
+        </button>
+        <button className="center" onClick={() => setPosition("Center")}>
+          Center
+        </button>
+        <button className="right" onClick={() => setPosition("Right")}>
+          Right
+        </button>
+        <div className={`gameArea gameArea${position}`}>
+          <div className="box"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
